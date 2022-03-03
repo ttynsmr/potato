09281ac48207570890b231a5a9d3f2e5bf6c4f66
Tetsuya Nishimura <ttynsmr@gmail.com>
2022-03-04 01:15:04 +0900

    maybe fixed

--- server/action-server3/generate_csharp_proto.sh
+++ server/action-server3/generate_csharp_proto.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
-protoc --proto_path=./proto --csharp_out=../../client/potato/Assets/Scripts/Proto ./proto/*.proto
-protoc --proto_path=./proto --csharp_out=../../client/potato/Assets/Scripts/Proto ./proto/generated/*/*.proto
 find ./proto -name "*.proto"
+mkdir -p ../../client/potato/Assets/Scripts/Proto
+# protoc --proto_path=./proto --csharp_out=../../client/potato/Assets/Scripts/Proto ./proto/*.proto
+# protoc --proto_path=./proto --csharp_out=../../client/potato/Assets/Scripts/Proto ./proto/generated/*/*.proto
+protoc --proto_path=./proto/generated --csharp_out=../../client/potato/Assets/Scripts/Proto ./proto/generated/*.proto
