6e6b602d940a6994c161e2926454619a5c3a9c0f
Tetsuya Nishimura <ttynsmr@gmail.com>
2022-03-18 12:23:26 +0900

    cloud functionsでpotato-instance-jp1のIPアドレスを取得できるようにした

---
+++ server/functions/get-hosts/entrypoint.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+
+var { listVMs } = require("./listVMs");
+
+var server = app.listen(3000, function(){
+  console.log("Node.js is listening to PORT:" + server.address().port);
+});
+
+//jq '.items."zones/asia-northeast1-b".instances[0].networkInterfaces[0].accessConfigs[0].natIP'
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await listVMs();
+    console.log(JSON.stringify(result))
+
+    if (result.items["zones/asia-northeast1-b"].instances[0].networkInterfaces[0].accessConfigs[0].natIP) {
+      res.status(200).send(result.items["zones/asia-northeast1-b"].instances[0].networkInterfaces[0].accessConfigs[0].natIP);
+    }
+    else {
+      res.status(404).send("");
+    }
+  } catch (error) {
+    res.status(404).send("");
+  }
+});
