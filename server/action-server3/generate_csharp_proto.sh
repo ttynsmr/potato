786b2afdbfaff06e7a17b26ea0de9abe587be048
Tetsuya Nishimura <ttynsmr@gmail.com>
2022-04-19 22:37:38 +0900

    クライアントのrpc生成スクリプトがプロジェクトに出力してたのでテンポラリに変更

--- server/action-server3/generate_csharp_proto.sh
+++ server/action-server3/generate_csharp_proto.sh
@@ -1,2 +1,14 @@
-find ./proto -name "*.proto"
-protoc --proto_path=./proto --csharp_out=../../client/potato/Assets/Scripts/Proto ./proto/*.proto
+#!/usr/bin/env bash
+
+basepath=$(basename "$0")
+tmpd=$(mktemp -dt "$basepath.XXXXXXXX")/
+echo "$tmpd"
+
+pushd torikime || exit
+pipenv run python torikime.py -v -o "$tmpd"proto -s "../../../client/potato/Assets/Scripts/Rpc/Generated" -i "../rpc" --cache_dir "$tmpd"cache
+popd || exit
+
+cp ./proto/*.proto "$tmpd"proto/
+
+find "$tmpd"proto -name "*.proto"
+protoc --proto_path="$tmpd"proto --csharp_out=../../client/potato/Assets/Scripts/Proto "$tmpd"proto/*.proto
